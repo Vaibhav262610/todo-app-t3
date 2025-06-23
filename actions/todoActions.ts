7f708d898044947216626b5c39317127c748db97
Vaibhav262610 <branda56235@gmail.com>
2025-06-23 15:39:00 +0530

    H

--- actions/todoActions.ts
+++ actions/todoActions.ts
@@ -1,45 +1,66 @@
 "use server";
+
 import { eq, not } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { db } from "@/db/drizzle";
 import { todo } from "@/db/schema";
 
 export const getData = async () => {
-  const data = await db.select().from(todo);
-  return data;
+  try {
+    const data = await db.select().from(todo);
+    return data;
+  } catch (err) {
+    console.error("Error in getData:", err);
+    throw err;
+  }
 };
 
 export const addTodo = async (id: number, text: string) => {
-  await db.insert(todo).values({
-    id: id,
-    text: text,
-  });
+  try {
+    await db.insert(todo).values({ id, text });
+    revalidatePath("/");
+  } catch (err) {
+    console.error("Error in addTodo:", err);
+    throw err;
+  }
 };
 
 export const deleteTodo = async (id: number) => {
-  await db.delete(todo).where(eq(todo.id, id));
-
-  revalidatePath("/");
+  try {
+    await db.delete(todo).where(eq(todo.id, id));
+    revalidatePath("/");
+  } catch (err) {
+    console.error("Error in deleteTodo:", err);
+    throw err;
+  }
 };
 
 export const toggleTodo = async (id: number) => {
-  await db
-    .update(todo)
-    .set({
-      done: not(todo.done),
-    })
-    .where(eq(todo.id, id));
-
-  revalidatePath("/");
+  try {
+    await db
+      .update(todo)
+      .set({
+        done: not(todo.done),
+      })
+      .where(eq(todo.id, id));
+    revalidatePath("/");
+  } catch (err) {
+    console.error("Error in toggleTodo:", err);
+    throw err;
+  }
 };
 
 export const editTodo = async (id: number, text: string) => {
-  await db
-    .update(todo)
-    .set({
-      text: text,
-    })
-    .where(eq(todo.id, id));
-
-  revalidatePath("/");
+  try {
+    await db
+      .update(todo)
+      .set({
+        text,
+      })
+      .where(eq(todo.id, id));
+    revalidatePath("/");
+  } catch (err) {
+    console.error("Error in editTodo:", err);
+    throw err;
+  }
 };
