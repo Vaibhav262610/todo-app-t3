a247f5356faaf2b99f9b890defabb6e0f10a719c
Vaibhav262610 <branda56235@gmail.com>
2025-06-23 12:06:59 +0530

    H

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,8 @@ const page = async () => {
     <div className='h-screen flex flex-col justify-center items-center gap-5 text-black bg-black '>
       {/* <button className='bg-green-300 p-2 rounded-2xl text-md font-semibold px-8' onClick={signUp}>Sign Up</button> */}
       {!session ? <p className='text-emerald-400 font-medium text-xl uppercase'>Not Signed In</p> : <p className='text-emerald-400 font-medium text-xl uppercase'>Hello {session.user.name}</p>}
-      <Link href='sign-in'>
-        <Button className='bg-red-300 hover:bg-green-500'>Sign In</Button>
+      <Link href={!session ? '/sign-in' : '/dashboard'}>
+      {!session ? <Button className='bg-red-300 hover:bg-green-500'>Sign In</Button> : <Button className='bg-red-300 hover:bg-green-500'>Dashboard</Button>}
       </Link>
     </div>
   )
